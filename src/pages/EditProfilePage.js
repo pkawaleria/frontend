d08c6df41111d87f0460b6d07e39931a55033f4e
Kamilek <104760585+KlonicaRadoslaw@users.noreply.github.com>
2023-09-15 13:19:24 +0200

    EditProfilePage, Account info update hook, Navbar update, Profile hook

---
+++ src/pages/EditProfilePage.js
@@ -0,0 +1,25 @@
+import Navbar from "../components/navbar/Navbar"
+import Footer from "../components/footer/Footer"
+import EditProfile from "../components/account/EditProfile"
+import ScrollToTopButton from "../components/functions/ScrollToTopButton"
+import ScrollToTop from '../components/functions/ScrollToTop'
+import { useFontSize } from "../components/themes/FontSizeContext"
+
+export function EditProfilePage() {
+
+    const { fontSize } = useFontSize();
+
+    const style = {
+        fontSize: fontSize === 'small' ? '16px' : fontSize === 'medium' ? '24px' : '24px',
+    };
+
+    return (
+        <div className="h-full flex flex-col" style={style}>
+            <Navbar />
+            <ScrollToTop />
+            <ScrollToTopButton />
+            <EditProfile />
+            <Footer />
+        </div>
+    )
+}
