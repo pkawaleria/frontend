6b8daa70d719f52de7a450e92a1f07a61b53a736
nqriver <nqriver@gmail.com>
2023-11-20 00:22:58 +0100

    PA-59 Working 'Add new auction' page

--- src/services/auctionsService.js
+++ src/services/auctionsService.js
@@ -1,5 +1,32 @@
 import auctionMsApi from './auctionMsApi';
 
+
+export const createAuction = async (payload) => {
+    try {
+        const response = await auctionMsApi.post(`/auction-service/auctions`, payload);
+        return response.data;
+    } catch (error) {
+        console.error("Error adding new auction", error);
+        throw error;
+    }
+};
+
+export const addImagesToAuction = async (auctionId, payload) => {
+    try {
+        const response = await auctionMsApi.post(`/auction-service/auctions/${auctionId}/images`, payload,
+            {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }
+        );
+        return response.data;
+    } catch (error) {
+        console.error("Error adding images to auction", error);
+        throw error;
+    }
+};
+
 export const fetchAllAuctions = async (pageNumber, pageSize) => {
     try {
         const response = await auctionMsApi.get(`/auction-service/auctions/search?page=${pageNumber}&pageSize=${pageSize}`);
@@ -17,7 +44,7 @@ export const searchAuctions = async (pageNumber, pageSize, filters) => {
         queryParams.page = pageNumber;
         queryParams.pageSize = pageSize;
 
-        const response = await auctionMsApi.get('/auction-service/auctions/search', { params: queryParams });
+        const response = await auctionMsApi.get('/auction-service/auctions/search', {params: queryParams});
         return response.data;
     } catch (error) {
         console.error("Error searching auctions:", error);
