9bd31f539479814886e9428a944ba52af18ec87f
Kamilek <radek71k@onet.pl>
2023-11-29 10:51:02 +0100

    Fixed dark mode for new auction confirmed popup

--- src/components/auctions/NewAuction.js
+++ src/components/auctions/NewAuction.js
@@ -472,13 +472,13 @@ export default function NewAuction() {
                 {messagePopup.show && (
                     <div className="fixed inset-0 flex items-center justify-center z-50">
                         <div className="absolute inset-0 bg-black opacity-50"></div>
-                        <div className="relative bg-white w-1/2 rounded-lg shadow-md p-8 opacity-100">
-                            <p className="text-lg font-semibold mb-4 text-center">
+                        <div className="relative bg-white dark:bg-neutral-600 w-1/2 rounded-lg shadow-md p-8 opacity-100">
+                            <p className="text-lg dark:text-neutral-200 font-semibold mb-4 text-center">
                                 ${messagePopup.message}
                             </p>
                             <div className="flex justify-center space-x-4">
                                 <button
-                                    className="text-white bg-blue-500 hover:bg-blue-700 py-2 px-4 rounded-md"
+                                    className="text-white dark:text-neutral-200 bg-blue-500 hover:bg-blue-700 dark:bg-blue-900 dark:hover:bg-blue-700 py-2 px-4 rounded-md"
                                     onClick={handlePopupClose}
                                 >
                                     Ok
